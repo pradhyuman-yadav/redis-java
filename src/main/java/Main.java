64ab2e1d00f9903082c10cdaa6bda735544c2c48
Yadav, Pradhyuman <pradhyuman.yadav@gwu.edu>
2024-03-08 17:07:08 -0800

    pass 2nd stage

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -25,7 +25,7 @@ public class Main {
           while (!serverSocket.isClosed()) {
               String command = commandReader.readLine();
               System.out.println("Command = " + command);
-              if (command.equalsIgnoreCase("PING")) writer.println("+PONG\\r\\n");
+              if (command.equalsIgnoreCase("PING")) writer.println("+PONG\r\n");
           }
 
         } catch (IOException e) {
