339fb399d53491e0875efd85e0ec99ce36882133
Yadav, Pradhyuman <pradhyuman.yadav@gwu.edu>
2024-03-08 19:22:48 -0800

    pass 3nd stage

--- src/main/java/ClientHandler.java
+++ src/main/java/ClientHandler.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.HashMap;
 
 public class ClientHandler extends Thread {
     private Socket socket;
@@ -16,11 +17,34 @@ public class ClientHandler extends Thread {
             BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             PrintWriter output = new PrintWriter(socket.getOutputStream(), true);
 
+            HashMap<String, String> storage = new HashMap<>();
+
             String text;
             while ((text = input.readLine()) != null) {
               System.out.println("Command = " + text);
                 if (text.equalsIgnoreCase("PING")) output.println("+PONG\r");
-                if (text.equalsIgnoreCase("ECHO")) if ((text= input.readLine()) != null) output.println("+"+input.readLine()+"\r");
+                else if (text.equalsIgnoreCase("ECHO")) {
+                    String nextTextLength;
+                    if ((nextTextLength = input.readLine()) != null && !nextTextLength.equalsIgnoreCase("-1")) {
+                        output.println("+" + input.readLine() + "\r");
+                    }
+                }
+                else if (text.equalsIgnoreCase("set")) {
+                    String nextTextLength;
+                    if ((nextTextLength = input.readLine()) != null && !nextTextLength.equalsIgnoreCase("-1")) {
+                        String setText = input.readLine();
+                        if((nextTextLength = input.readLine()) != null && !nextTextLength.equalsIgnoreCase("-1")) {
+                            String setData = input.readLine();
+                            storage.put(setText, setData);
+                        }
+                    }
+                }
+                else if (text.equalsIgnoreCase("get")) {
+                    String nextTextLength;
+                    if ((nextTextLength = input.readLine()) != null && !nextTextLength.equalsIgnoreCase("-1")) {
+                        output.println("+" + storage.get(input.readLine()) + "\r");
+                    }
+                }
             }
 
             socket.close();
